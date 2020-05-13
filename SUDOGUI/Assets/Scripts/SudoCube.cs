fdfa8929adf47d7d14444826446ed6138686822c
rosco-y <ross.ylitalo@gmail.com>
2020-05-13 11:23:29 -0400

    Have the 3D array of SudoCubes constructing by GameManager.  Everything looks great--I'm planning on making the text on the buttons on UNK a little bigger to make them easier to read.

--- SUDOGUI/Assets/Scripts/SudoCube.cs
+++ SUDOGUI/Assets/Scripts/SudoCube.cs
@@ -1,22 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class SudoCube : MonoBehaviour
 {
-
+    // Start is called before the first frame update
+    Transform _camera;
+    int _sudoValue;
     bool[] _buttonEnabled;
-
-    private void Start()
+    bool _sudoHole = false;
+    int _sudoSolution;
+    void Start()
     {
-        _buttonEnabled = new bool[10];
-        for (int i = 0; i < 10; i++)
+        _camera = Camera.main.transform;
+        _buttonEnabled = new bool[g.PUZZLESIZE + 1];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(_camera);
+        transform.rotation = _camera.rotation;
+    }
+
+    public void ButtonClick(Button button)
+    {
+
+    }
+
+    public int SudoValue
+    {
+        set
         {
-            _buttonEnabled[i] = false;
+            _sudoValue = value;
+            _sudoHole = (value < 0);
+            _sudoSolution = Mathf.Abs(value);
+        }
+        get
+        {
+            return _sudoValue;
         }
     }
+
+    public int SudoSolution
+    {
+        get
+        {
+            return _sudoSolution;
+        }
+    }
+
+    /// <summary>
+    /// true if this was a cell that the user needed to solve.
+    /// </summary>
+    public bool SudoHole
+    {
+        get
+        {
+            return _sudoHole;
+        }
+    }
+
     public void ButtonClick(TMPro.TMP_Text text)
     {
         int buttonNo = int.Parse(text.text);
