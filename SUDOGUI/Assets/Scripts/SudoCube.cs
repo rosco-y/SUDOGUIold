4c96ad1a68e5ead77dc1761c5cecb7c978028f01
rosco-y <ross.ylitalo@gmail.com>
2020-05-13 10:32:35 -0400

    Have a pretty sweet UNK cube now--nice looking buttons that all use the same Button_Click() method to Toggle a button's "Selected" status (bool). Also created the 1,2,3...7,8,9 SudoCubes.  I believe I am about ready for the GameManager to build the 9x9x9 cube array of SudoCubes.

---
+++ SUDOGUI/Assets/Scripts/SudoCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SudoCube : MonoBehaviour
+{
+
+    bool[] _buttonEnabled;
+
+    private void Start()
+    {
+        _buttonEnabled = new bool[10];
+        for (int i = 0; i < 10; i++)
+        {
+            _buttonEnabled[i] = false;
+        }
+    }
+    public void ButtonClick(TMPro.TMP_Text text)
+    {
+        int buttonNo = int.Parse(text.text);
+        _buttonEnabled[buttonNo] = !_buttonEnabled[buttonNo];
+        if (_buttonEnabled[buttonNo])
+            text.color = Color.black;
+        else
+            text.color = Color.grey;
+    }
+}
